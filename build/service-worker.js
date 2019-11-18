3ebcc1372723f12ec596e4420a5971b79e522b67
Emmanuel <meric.emmanuel@gmail.com>
2019-11-18 14:22:24 +0100

    built

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a66ad59551c04fe9efd42d38e1c1c9eb.js"
+  "/precache-manifest.a0a3c4b204da61473edcfd75e07188bd.js"
 );
 
 self.addEventListener('message', (event) => {
