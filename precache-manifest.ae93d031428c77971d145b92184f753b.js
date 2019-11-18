013080825a39a6bd9ec3180aa419629422f0041e
Emmanuel <meric.emmanuel@gmail.com>
2019-11-18 16:31:21 +0100

    Updates

---
+++ precache-manifest.ae93d031428c77971d145b92184f753b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf807e7be32a7e0cb54487518dcf671b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eec8cc90ad344f39bce5",
+    "url": "/static/css/main.b690864a.chunk.css"
+  },
+  {
+    "revision": "ac4d474d09206267d50d",
+    "url": "/static/js/2.89463856.chunk.js"
+  },
+  {
+    "revision": "eec8cc90ad344f39bce5",
+    "url": "/static/js/main.26a9094a.chunk.js"
+  },
+  {
+    "revision": "453f799caa661711295f",
+    "url": "/static/js/runtime-main.2a26a57f.js"
+  }
+]);
