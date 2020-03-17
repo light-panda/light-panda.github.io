4ebf39aa89a42d9b58b794d1c15554d2b29f2737
Emmanuel <meric.emmanuel@gmail.com>
2020-03-17 16:31:20 +0100

    delete animation with react-spring

--- src/components/Todos/TodoItem.js
+++ src/components/Todos/TodoItem.js
@@ -1,28 +1,16 @@
-import {CSSTransition, Transition} from "react-transition-group";
+import {CSSTransition} from "react-transition-group";
 import React, {useState} from "react";
 import Toolbox from "../Toolbox/Toolbox";
 import classNames from 'classnames';
 import mobile from "is-mobile";
 import {Draggable} from "react-beautiful-dnd";
 import Editable from "../Editable";
-
-const duration = 400
-
-const defaultStyle = {
-    transition: `top ${duration}ms ease-in-out, left ${duration}ms ease-in-out, opacity ${duration}ms ease-in-out`,
-    opacity: 1,
-    position: 'relative',
-    top: '0px',
-    left: '0px'
-}
-
-const transitionStyles = {
-    entering: {opacity: 0, top: '-20px', left: '0px'},
-};
+import {useSpring, animated} from 'react-spring';
+import useMeasure from "../../utils/useMeasure";
 
 const callEach = (...functions) => {
     return (el) => {
-        functions.forEach(func => func(el))
+        functions.forEach(func => console.log(func) || typeof func === 'function' && func(el))
     }
 }
 
@@ -36,11 +24,18 @@ function useHover() {
 }
 
 
-const  TodoItem = React.forwardRef(({todo, itemsBeingDeleted, deleteItem, toggleItemSelection, index, setEditable, editItem}, ref) => {
+const  TodoItem = React.forwardRef(({todo, deleteItem, toggleItemSelection, index, setEditable, editItem}) => {
     const [hoverProps, isHovered] = useHover()
+    const [deleting, setDeleting] = useState(false)
     const [hoverToolboxProps, isToolboxHovered] = useHover()
     const [extended, setExtended] = useState(false)
 
+    const [{ref}, { height }] = useMeasure()
+
+    // list__item has hardcoded padding: 8px so we put + 2*8 here to take padding into account
+    // the height value comes from the ResizeObserver API so we can't get the padding value, nor box-sizing: border-box;
+    // nor replace the padding by a margin because it would break drag&drop behavior
+    const deletingStyle = useSpring({opacity: deleting ? 0 : 1, marginTop: deleting ? -(height + 2*8) : 0})
 
     const handleTouchStart = (event) => {
         event.preventDefault();
@@ -51,29 +46,29 @@ const  TodoItem = React.forwardRef(({todo, itemsBeingDeleted, deleteItem, toggle
     return (
         <Draggable key={todo.id} draggableId={todo.id} index={index} isDragDisabled={mobile() ? false : (!isHovered || isToolboxHovered)}>
             {(provided) => (
-                <Transition in={!!itemsBeingDeleted.find(item => item.id === todo.id)}
-                            timeout={duration} onEntered={() => deleteItem({id: todo.id})}>
-                    {deletingState => (
-                        <div className={'list__item'} ref={el => callEach(ref,provided.innerRef)(el)} {...provided.draggableProps} {...provided.dragHandleProps}>
-                            <div className={'list__item__sticker'} {...hoverProps}>
-                                <CSSTransition in={mobile() ? todo.selected : (isHovered || (isToolboxHovered && extended))} timeout={{enter: 0, exit: mobile() ? 0 : 500}}
-                                               classNames={'list__item__toolbox'}
-                                               onEntered={() => setExtended(true)}
-                                               onExited={() => setExtended(false)}>
-                                    {() => (
-                                        <>
-                                            <Toolbox className={'list__item__toolbox'} innerProps={hoverToolboxProps} onDelete={deleteItem} onEdit={() => setEditable(true)}/>
-                                            <li onTouchStart={handleTouchStart} className={classNames('list__item__content', (mobile() && todo.selected || !mobile() && isHovered && false) && 'list__item__content-selected')}
-                                                style={{...defaultStyle, ...transitionStyles[deletingState]}}>
-                                                <Editable value={todo.text} editable={todo.editable} onSave={editItem} setEditable={setEditable}/>
-                                            </li>
-                                        </>
-                                    )}
-                                </CSSTransition>
-                            </div>
-                        </div>
-                    )}
-                </Transition>
+                <animated.div className={'list__item'}
+                              ref={el => {ref.current = el; provided.innerRef(el)}}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              style={{...provided.draggableProps.style, ...provided.dragHandleProps ? provided.dragHandleProps.style : {}, ...deletingStyle}}
+                >
+                    <div className={'list__item__sticker'} {...hoverProps}>
+                        <CSSTransition in={mobile() ? todo.selected : (isHovered || (isToolboxHovered && extended))} timeout={{enter: 0, exit: mobile() ? 0 : 500}}
+                                       classNames={'list__item__toolbox'}
+                                       onEntered={() => setExtended(true)}
+                                       onExited={() => setExtended(false)}>
+                            {() => (
+                                <>
+                                    <Toolbox className={'list__item__toolbox'} innerProps={hoverToolboxProps} onDelete={() => setDeleting(true)} onEdit={() => setEditable(true)}/>
+                                    <li onTouchStart={handleTouchStart}
+                                        className={classNames('list__item__content', mobile() && todo.selected && 'list__item__content-selected')}>
+                                        <Editable value={todo.text} editable={todo.editable} onSave={editItem} setEditable={setEditable}/>
+                                    </li>
+                                </>
+                            )}
+                        </CSSTransition>
+                    </div>
+                </animated.div>
             )}
         </Draggable>
     )
--- src/components/Todos/Todos.js
+++ src/components/Todos/Todos.js
@@ -3,7 +3,6 @@ import './Todos.css'
 import {randomHash} from "../../utils/random";
 import {useLocalStorage} from "use-hooks";
 import TodoItem from "./TodoItem";
-import QueueAnim from 'rc-queue-anim';
 import {DragDropContext, Droppable} from "react-beautiful-dnd";
 
 const reorder = (list, startIndex, endIndex) => {
@@ -16,17 +15,13 @@ const reorder = (list, startIndex, endIndex) => {
 
 function Todos() {
   const [items, setItems] = useLocalStorage('todos', [])
-  const [itemsBeingDeleted, setItemsBeingDeleted] = useState([])
 
   const [value, setValue] = useState('')
-
-  // removing eventual corrupted state elements
-  useEffect(() => setItems(items.filter(item => item.deleting !== true)), [])
 
   const handleKeyDown = ({key}) => {
     if (key === 'Enter' && value.length > 0) {
       // add new item
-      setItems([{id: randomHash(), text: value, done: false, deleting: false, editable: false}, ...items])
+      setItems([{id: randomHash(), text: value, editable: false}, ...items])
       // empty text input
       setValue('')
     }
@@ -40,7 +35,6 @@ function Todos() {
   }
 
   const editItem = ({id, text}) => {
-    console.log({id, text})
     setItems(items.map(item => item.id === id
         ? {...item, text, editable: false}
         : {...item, editable: false})
@@ -53,13 +47,6 @@ function Todos() {
     ])
   }
 
-  const beginDelete = ({id}) => {
-    setItemsBeingDeleted([
-      ...itemsBeingDeleted,
-      {id}
-    ])
-  }
-
   const toggleItemSelection = ({id}) => {
     setItems(items.map(item => item.id === id
       ? {...item, selected:!item.selected}
@@ -69,7 +56,6 @@ function Todos() {
 
   const deselectAllItems = () => {
     setItems(items.map(item => ({...item, selected:false})))
-    console.log('hi');
   }
 
   const onDragEnd = (result) => {
@@ -93,12 +79,9 @@ function Todos() {
         <Droppable droppableId={'droppable'}>
           {(provided) => (
             <div  {...provided.droppableProps} ref={provided.innerRef} className={'list-droppable'}>
-              <QueueAnim component={'ul'} className={'list'} type={['top', 'top']} onTouchStart={deselectAllItems}
-                         appear={false}>
                 {items.map((todo, index) => (
                   <TodoItem todo={todo}
                             key={todo.id}
-                            itemsBeingDeleted={itemsBeingDeleted}
                             index={index}
                             deleteItem={() => deleteItem({id: todo.id})}
                             setEditable={(editable) => setEditable({id: todo.id, editable})}
@@ -106,7 +89,6 @@ function Todos() {
                             toggleItemSelection={() => toggleItemSelection(todo)}/>
                 ))}
                 {provided.placeholder}
-              </QueueAnim>
             </div>
           )}
         </Droppable>
